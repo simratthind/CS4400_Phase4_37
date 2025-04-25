aa5047aa576a86777ee4c0353dc08bf1300e242b
Nikki Lakhotia <nlakhotia6@gatech.edu>
2025-04-24 21:28:52 -0400

    message

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,6 +5,10 @@ from .person import person_bp
 from .license import license_bp
 from .flight import flight_bp
 from .views import views_bp
+from .crew import crew_bp
+from .retire import retire_bp
+from .onground import onground_bp
+
 
 def register_routes(app):
     app.register_blueprint(airplane_bp)
@@ -13,4 +17,8 @@ def register_routes(app):
     app.register_blueprint(license_bp)
     app.register_blueprint(flight_bp)
     app.register_blueprint(views_bp)
+    app.register_blueprint(crew_bp)
+    app.register_blueprint(retire_bp)
+    app.register_blueprint(onground_bp)
+
 
